6048b04d5efb8de2557521b0873fc0af4af7a70e
zhm09 <1026673497@qq.com>
2017-01-15 14:08:23 +0800

    Update Timeline.swift

--- Record/Record/Timeline.swift
+++ Record/Record/Timeline.swift
@@ -1,10 +1,4 @@
-//
-//  Timeline.swift
-//  TimelineTableViewCell
-//
-//  Created by Zheng-Xiang Ke on 2016/10/21.
-//  Copyright © 2016年 Zheng-Xiang Ke. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
