461f6ffe2a98f8199e1a9baa381dfb0d52dd82b1
zhm09 <1026673497@qq.com>
2017-01-15 14:08:40 +0800

    Update TimelinePoint.swift

--- Record/Record/TimelinePoint.swift
+++ Record/Record/TimelinePoint.swift
@@ -1,10 +1,4 @@
-//
-//  TimelinePoint.swift
-//  TimelineTableViewCell
-//
-//  Created by Zheng-Xiang Ke on 2016/10/20.
-//  Copyright © 2016年 Zheng-Xiang Ke. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
