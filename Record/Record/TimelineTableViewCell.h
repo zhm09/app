7ca8d28b4da500d373fd9c4d97ca61bd79f09b4f
zhm09 <1026673497@qq.com>
2017-01-15 14:09:06 +0800

    Update TimelineTableViewCell.h

--- Record/Record/TimelineTableViewCell.h
+++ Record/Record/TimelineTableViewCell.h
@@ -1,10 +1,4 @@
-//
-//  TimelineTableViewCell.h
-//  TimelineTableViewCell
-//
-//  Created by Zheng-Xiang Ke on 2016/10/20.
-//  Copyright © 2016年 Zheng-Xiang Ke. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
